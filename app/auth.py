34db7a92eb9e4cae4d958c42fc5581b69819db3e
Caua theodoro <caua82477@gmail.com>
2026-05-05 16:53:07 -0300

    dependencia fastAPI

--- app/auth.py
+++ app/auth.py
@@ -44,3 +44,29 @@ def criar_token(data: dict):
 def decodificar_token(token: str):
     payload = jwt.decode(token, SECRETY_KEY, algorithms=[ALGORITHM])
     return payload
+
+#Dependencias do FastAPI
+def get_usuario_logado(request: Request):
+
+    token = request.cookies.get("access_token")
+
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Não autenticado"
+        )
+
+    try:
+        payload = decodificar_token(token)
+        email = payload.get("sub")
+        if email is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token inválido"
+            )
+        return payload
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token Inválido ou expirado"
+        )
